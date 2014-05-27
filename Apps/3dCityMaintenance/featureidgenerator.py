ed89afcc217cdf90e249e55cd1747c03805e2b7a
Thorsten Reitz <treitz@esri.com>
2014-05-27 16:08:45 +0200

    Updated featureidgenerator.py to be compatible with 3DCIM 1.4 and the ArcGIS attribute assistant extension

--- Apps/3dCityMaintenance/featureidgenerator.py
+++ Apps/3dCityMaintenance/featureidgenerator.py
@@ -8,7 +8,7 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
 # ------------------------------------------------------------------------------
 
@@ -36,26 +36,26 @@ class FeatureIdGenerator(object):
 
         # Generation Method Field parameter
         generation_field = arcpy.Parameter(
-            displayName="Generation Method",
-            name="generation_method",
+            displayName="3DCIM Schema Version",
+            name="schema_version",
             datatype="String",
             parameterType="Required",
             direction="Input")
 
         # Set a value list for the Generation method
         generation_field.filter.type = "ValueList"
-        generation_field.filter.list = ["Global Hi-Lo Counter"]
-        generation_field.value = "Global Hi-Lo Counter"
+        generation_field.filter.list = ["1.3", "1.4"]
+        generation_field.value = "1.4"
 
-        # Hi Batchsize Method Field parameter
+        # Interval Size Field parameter
         hi_batchsize_field = arcpy.Parameter(
-            displayName="Hi batch size",
+            displayName="Interval size",
             name="hi_batchsize",
             datatype="Long",
             parameterType="Required",
             direction="Input")
 
-        hi_batchsize_field.value = 10000
+        hi_batchsize_field.value = 20000
 
         # Derived Output Features parameter
         out_gdb = arcpy.Parameter(
@@ -85,30 +85,45 @@ class FeatureIdGenerator(object):
         """Modify the messages created by internal validation for each tool
         parameter.  This method is called after internal validation."""
         return
-        
+
     def execute(self, parameters, messages):
         """The source code of the tool."""
 
         arcpy.env.workspace = parameters[0].value
+        schema_version = parameters[1].value
 
         # Number of low IDs per hi ID
         # Higher batch sizes mean less updating of the table, lower batch sizes more
         # efficient ID usage especially when multiple processes access the table.
         hi_batchsize = parameters[2].value
 
-        # Name of the table used to maintain hi/lo counter status per feature class. Could also become a parameter.
+        # Name of the table used to maintain hi/lo counter status per feature class. Value depends on schema version.
         generate_ID_table_name = "GenerateID"
+        seqnameField = "name"
+        seqcounterField = "hi"
+        seqintervalField = "low"
+        if schema_version == "1.4":
+            generate_ID_table_name = "GenerateId"
+            seqnameField = "SEQNAME"
+            seqcounterField = "SEQCOUNTER"
+            seqintervalField = "SEQINTERV"
 
-        # check whether sequences table has already been created.
+        # check whether sequences table has already been created and create if not.
         new_table = None
         counter_tbl_list = arcpy.ListTables(generate_ID_table_name)
         if not counter_tbl_list:
-            arcpy.AddMessage("Creating new GenerateID table.")
+            arcpy.AddMessage("Creating new " + generate_ID_table_name +" table.")
             new_table = True
             generate_ID_table = arcpy.CreateTable_management(arcpy.env.workspace, generate_ID_table_name)
-            arcpy.AddField_management(generate_ID_table, "name", "TEXT", None, None, 50, "Feature Class Name", "NON_NULLABLE", "REQUIRED")
-            arcpy.AddField_management(generate_ID_table, "hi", "LONG", None, None, None, "Hi counter", "NON_NULLABLE", "REQUIRED")
-            arcpy.AddField_management(generate_ID_table, "low", "LONG", None, None, None, "Low counter", "NON_NULLABLE", "REQUIRED")
+            if schema_version == "1.3":
+                arcpy.AddField_management(generate_ID_table, seqnameField, "TEXT", None, None, 50, "Feature Class Name", "NON_NULLABLE", "REQUIRED")
+                arcpy.AddField_management(generate_ID_table, seqcounterField, "LONG", None, None, None, "Hi counter", "NON_NULLABLE", "REQUIRED")
+                arcpy.AddField_management(generate_ID_table, seqintervalField, "LONG", None, None, None, "Low counter", "NON_NULLABLE", "REQUIRED")
+            if schema_version == "1.4": # identical schema to attribute assistant
+                arcpy.AddField_management(generate_ID_table, seqnameField, "TEXT", None, None, 50, "Sequence Name", "NON_NULLABLE", "NON_REQUIRED")
+                arcpy.AddField_management(generate_ID_table, seqcounterField, "LONG", None, None, None, "Sequence Counter", "NON_NULLABLE", "NON_REQUIRED")
+                arcpy.AddField_management(generate_ID_table, seqintervalField, "SHORT", None, None, None, "Interval Value", "NULLABLE", "NON_REQUIRED")
+                arcpy.AddField_management(generate_ID_table, "COMMENTS", "LONG", None, None, 255, "Comments", "NULLABLE", "NON_REQUIRED")
         else:
             new_table = False
             generate_ID_table = counter_tbl_list[0]
@@ -117,32 +132,45 @@ class FeatureIdGenerator(object):
         fc_list = arcpy.ListFeatureClasses()
         for fc in fc_list:
             arcpy.AddMessage("Processing " + fc)
-            hi_counter = 0
-            low_counter = 0
+            counter = 0 # counter in this session, range is always 0 ... [interval - 1]
+            baseCount = 0 # value
+            interval = hi_batchsize # batchsize/interval size
 
             # if we only created the GenerateID table, we know we have to insert the counter.
             if new_table:
-                insert_new_counter_cursor = arcpy.da.InsertCursor(generate_ID_table_name, ["name", "hi", "low"])
-                insert_new_counter_cursor.insertRow((fc, 0, 0))
+                insert_new_counter_cursor = arcpy.da.InsertCursor(generate_ID_table_name, [seqnameField, seqcounterField, seqintervalField])
+                insert_new_counter_cursor.insertRow((fc, 0, hi_batchsize))
                 del insert_new_counter_cursor
 
             # check if a counter of fc_name exists and retrieve value
-            with arcpy.da.SearchCursor(generate_ID_table_name, ["name", "hi", "low"]) as rows:
+            counterParams = None
+            escaped_name = arcpy.AddFieldDelimiters(generate_ID_table_name, seqnameField)
+            where_clause = escaped_name + " = " + "'" + fc + "'"
+            with arcpy.da.SearchCursor(generate_ID_table_name, [seqnameField, seqcounterField, seqintervalField], where_clause) as rows:
+                for counterRow in rows:
+                    counterParams = counterRow
+                    break
+
+            if counterParams != None:
+                baseCount = counterParams[1]
+                interval = counterParams[2]
+            else:
+                # create that counter
+                insert_new_counter_cursor = arcpy.da.InsertCursor(generate_ID_table_name, [seqnameField, seqcounterField, seqintervalField])
+                insert_new_counter_cursor.insertRow((fc, 0, hi_batchsize))
+                del insert_new_counter_cursor
+
+            with arcpy.da.SearchCursor(generate_ID_table_name, [seqnameField, seqcounterField, seqintervalField]) as rows:
                 for row in rows:
                     if row[0] == fc:
-                        hi_counter = row[1]
-                        low_counter = row[2]
+                        baseCount = row[1]
+                        interval = row[2]
                         break
 
-            # increment hi counter to indicate that it is in active usage
-            with arcpy.da.UpdateCursor(generate_ID_table_name, ["name", "hi"]) as rows:
-                for row in rows:
-                    if row[0] == fc:
-                        row[1] = 1 + hi_counter
-                        rows.updateRow(row)
-                        break
+            # increment counter to indicate that it is in active usage
+            self.incrementCounter(generate_ID_table_name, seqnameField, seqcounterField, fc, baseCount + interval)
 
-            # check if feature class alread has a FID, add it if not.
+            # check if feature class already has a FID, add it if not.
             fid_name = fc + "FID"
             fields_list = arcpy.ListFields(fc, fid_name)
             if not fields_list:
@@ -152,25 +180,36 @@ class FeatureIdGenerator(object):
             with arcpy.da.UpdateCursor(fc, [fid_name]) as rows:
                 for row in rows:
                     if row[0] == None:
-                        if low_counter >= hi_batchsize:
-                            # update hi_counter, reset low_counter
-                            arcpy.AddMessage("Hi Sequence " + str(hi_counter) + " exhausted, using next Sequence.")
-                            escaped_name = arcpy.AddFieldDelimiters(generate_ID_table_name, "name")
-                            where_clause = escaped_name + " = " + "'" + fc + "'"
-                            new_hi_row = [row[0] for rows in arcpy.da.SearchCursor(generate_ID_table_name, ["hi"], where_clause)]
-                            hi_counter = new_hi_row[0]
-                            low_counter = 0
-                        row[0] = fc + "/" + str(hi_counter * hi_batchsize + low_counter)
-                        low_counter += 1
+                        if counter >= interval:
+                            # get new baseCount from GenerateId
+                            arcpy.AddMessage("Interval exhausted, getting next Interval.")
+                            with arcpy.da.SearchCursor(generate_ID_table_name, [seqcounterField], where_clause) as rows:
+                                for counterRow in rows:
+                                    baseCount = counterRow[0]
+                                    break
+
+                            # Reset local counter
+                            counter = 0
+                        row[0] = fc + "/" + str(baseCount + counter)
+                        counter += 1
                         rows.updateRow(row)
 
-            # write back the new low value to the GenerateID table.
-            with arcpy.da.UpdateCursor(generate_ID_table_name, ["name", "low"]) as rows:
+            # write back the new counter value to the GenerateID table.
+            with arcpy.da.UpdateCursor(generate_ID_table_name, [seqnameField, seqcounterField]) as rows:
                 for newRow in rows:
                     if newRow[0] == fc:
-                        newRow[1] = low_counter
+                        newRow[1] = baseCount + counter
                         rows.updateRow(newRow)
                         break
 
         arcpy.AddMessage("Completed adding of Feature IDs.")
         return
+
+    def incrementCounter(self, generate_ID_table_name, seqnameField, seqcounterField, fcName, newCount):
+        # update counter in GenerateId table
+        with arcpy.da.UpdateCursor(generate_ID_table_name, [seqnameField, seqcounterField]) as rows:
+            for row in rows:
+                if row[0] == fcName:
+                    row[1] = newCount
+                    rows.updateRow(row)
+                    break
